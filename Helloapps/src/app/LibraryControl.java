87603a02ee69a48aa375f2affc9d48d188cdda27
monikakowalewska <36269925+monikakowalewska@users.noreply.github.com>
2018-09-12 16:12:51 +0200

    Revert "saving to file"
    This reverts commit ac1e4ec7df60433065b5999da64526eb281caae6.

--- Helloapps/src/app/LibraryControl.java
+++ Helloapps/src/app/LibraryControl.java
@@ -86,39 +86,5 @@ public class LibraryControl {
 	private void printMagazines() {
 		LibraryUtils.printMagazines(library);
 	}
-	//klasa wewnêtrzna 
-	private enum Option {
-        EXIT(0, "Wyjœcie z programu"),
-        ADD_BOOK(1, "Dodanie ksi¹¿ki"),
-        ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
-        PRINT_BOOKS(3, "Wyœwietlenie dostêpnych ksi¹¿ek"),
-        PRINT_MAGAZINES(4, "Wyœwietlenie dostêpnych magazynów/gazet");
-     
-        private int value;
-        private String description;
-     
-        Option(int value, String desc) {
-            this.value = value;
-            this.description = desc;
-        }
-         
-        @Override
-        public String toString() {
-            return value + " - " + description;
-        }
-        /*Metoda createFromInt() pozwala przekszta³ciæ wartoœæ typu int na odpowiedni¹ wartoœæ typu Option. 
-    	 * Wykorzystujemy tutaj metodê values(), która zwraca tablicê wszystkich wartoœci, 
-    	 * a poniewa¿ kolejne elementy numerujemy od 0, to zwracamy po prostu odpowiedni element tej tablicy.*/
-        public static Option createFromInt(int option) throws NoSuchElementException {
-            Option result = null;
-            try {
-                result = Option.values()[option];
-            } catch(ArrayIndexOutOfBoundsException e) {
-                throw new NoSuchElementException("Brak elementu o wskazanym ID");
-            }
-             
-            return result;
-        }
-    }
-}
 
+}

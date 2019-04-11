dbf1e5444b3c8e1ab6d77f5a47c8f87fead1792e
monikakowalewska <36269925+monikakowalewska@users.noreply.github.com>
2019-04-11 13:18:58 +0200

    point

--- MetodyKonstruktory/src/SpacyfikatoryDostępu2/PointApplication.java
+++ MetodyKonstruktory/src/SpacyfikatoryDostępu2/PointApplication.java
@@ -7,6 +7,12 @@ public class PointApplication {
         Point p=new Point(15,17);
         PointController pc=new PointController();
         pc.addX(p);
-        System.out.println("Po zwiêkszeniu x= "+p.getX());
+        System.out.println("Po zwiêkszeniu x, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
+        pc.minusX(p);
+        System.out.println("Po zmniejszeniu x, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
+        pc.addY(p);
+        System.out.println("Po zwiêkszeniu y, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
+        pc.minusY(p);
+        System.out.println("Po zmniejszeniu y, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
     }
 }

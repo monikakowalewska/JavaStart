fff36d9fb755a2d6672651ee94cf4af26e94b386
monikakowalewska <36269925+monikakowalewska@users.noreply.github.com>
2019-04-12 10:26:33 +0200

    point

--- MetodyKonstruktory/src/SpacyfikatoryDostępu2/PointApplication.java
+++ MetodyKonstruktory/src/SpacyfikatoryDostępu2/PointApplication.java
@@ -3,16 +3,39 @@ package SpacyfikatoryDostêpu2;
 import SpecyfikatoryDostêpu.Point;
 
 public class PointApplication {
+    public static final int ADD_X = 1;
+    public static final int MINUS_X = 2;
+    public static final int ADD_Y = 3;
+    public static final int MINUS_Y = 4;
     public static void main(String[] args) {
+
         Point p=new Point(15,17);
+        System.out.println("Punkt przed zmian¹: (" + p.getX()+";"+p.getY()+")");
+
         PointController pc=new PointController();
-        pc.addX(p);
-        System.out.println("Po zwiêkszeniu x, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
-        pc.minusX(p);
-        System.out.println("Po zmniejszeniu x, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
-        pc.addY(p);
-        System.out.println("Po zwiêkszeniu y, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
-        pc.minusY(p);
-        System.out.println("Po zmniejszeniu y, wspó³rzêdne punktu to "+p.getX()+" "+p.getY());
+        int option=3;
+        switch (option) {
+            case ADD_X:
+                pc.addX(p);
+                System.out.println("Po zwiêkszeniu x, wspó³rzêdne punktu to " + p.getX() + " " + p.getY());
+                break;
+            case MINUS_X:
+                pc.minusX(p);
+                System.out.println("Po zmniejszeniu x, wspó³rzêdne punktu to " + p.getX() + " " + p.getY());
+                break;
+            case ADD_Y:
+                pc.addY(p);
+                System.out.println("Po zwiêkszeniu y, wspó³rzêdne punktu to " + p.getX() + " " + p.getY());
+                break;
+            case MINUS_Y:
+                pc.minusY(p);
+                System.out.println("Po zmniejszeniu y, wspó³rzêdne punktu to " + p.getX() + " " + p.getY());
+                break;
+                default:
+                    System.out.println("Nie wykonano ¿ednej operacji.");
+        }
+        System.out.println("Punkt po zmianie: (" + p.getX()+";"+p.getY()+")");
+
     }
 }
+
